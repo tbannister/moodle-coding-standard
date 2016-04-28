cdd9ed6ec22b6164c014b14e3beaad35150bda0e
Derek Henderson <derek.henderson@remote-learner.net>
2016-04-28 09:46:09 -0400

    POET-16 If Superglobal detected, and if its just being checked, should not         return a positive.

--- POET/Sniffs/Security/SuperglobalSniff.php
+++ POET/Sniffs/Security/SuperglobalSniff.php
@@ -29,31 +29,40 @@ class POET_Sniffs_Security_SuperglobalSniff extends PHP_CodeSniffer_Standards_Ab
     /**
      * Member variables are never superglobals so this will always pass.
      *
-     * @param PHP_CodeSniffer_File $phpcsFile The file being scanned.
-     * @param int                  $stackPtr  The position of the current token in the stack passed in $tokens.
+     * @param PHP_CodeSniffer_File $phpcsfile The file being scanned.
+     * @param int                  $stackptr  The position of the current token in the stack passed in $tokens.
      */
-    protected function processMemberVar(PHP_CodeSniffer_File $phpcsFile, $stackPtr) {
+    protected function processMemberVar(PHP_CodeSniffer_File $phpcsfile, $stackptr) {
     }
 
     /**
      * Check if this variable is superglobal.
      *
-     * @param PHP_CodeSniffer_File $phpcsFile The file being scanned.
-     * @param int                  $stackPtr  The position of the current token in the stack passed in $tokens.
+     * @param PHP_CodeSniffer_File $phpcsfile The file being scanned.
+     * @param int                  $stackptr  The position of the current token in the stack passed in $tokens.
      */
-    protected function processVariable(PHP_CodeSniffer_File $phpcsFile, $stackPtr) {
-        $tokens = $phpcsFile->getTokens();
-        $varname = ltrim($tokens[$stackPtr]['content'], '$');
-
+    protected function processVariable(PHP_CodeSniffer_File $phpcsfile, $stackptr) {
+        $tokens = $phpcsfile->getTokens();
+        $varname = ltrim($tokens[$stackptr]['content'], '$');
         $superglobals = array(
                 'GLOBALS', '_SERVER', '_GET', '_POST', '_FILES', '_COOKIE', '_SESSION', '_REQUEST', '_ENV',
         );
 
+        $validcases = array(
+                'isset', 'header', 'unset', 'is_null', 'defined', 'empty', '__isset',
+                'array_key_exists',
+        );
         // If it's a superglobal, it's a potential security hole.
         if (in_array($varname, $superglobals) === true) {
-            $error = 'Superglobal %s detected.  Direct access to superglobals usually indicates a critical security problem.';
-            $data = array($varname);
-            $phpcsFile->addError($error, $stackPtr, 'Superglobal', $data);
+            if ($stackptr > 2) {
+               $preglobal = strtolower($tokens[$stackptr - 2]['content']);
+               $preglobal = trim($preglobal, '"');
+            }
+            if ((in_array($preglobal, $validcases) === false) || (stackptr < 2)) {
+               $error = 'Superglobal %s detected.  Direct access to superglobals usually indicates a critical security problem.';
+               $data = array($varname);
+               $phpcsfile->addError($error, $stackptr, 'Superglobal', $data);
+            }
         }
 
         $longarrays = array(
@@ -67,18 +76,18 @@ class POET_Sniffs_Security_SuperglobalSniff extends PHP_CodeSniffer_Standards_Ab
                      ' were deprecated as of PHP 5.0.0 and removed in PHP 5.4.0, presenting a potentially functionality'.
                      ' issue as well.';
             $data = array($varname);
-            $phpcsFile->addError($error, $stackPtr, 'Superglobal', $data);
+            $phpcsfile->addError($error, $stackptr, 'Superglobal', $data);
         }
 
     }
 
     /**
      * Processes the variable found within a double quoted string.
-     *
+
      * @param PHP_CodeSniffer_File $phpcsFile The file being scanned.
      * @param int                  $stackPtr  The position of the double quoted string.
      */
-    protected function processVariableInString(PHP_CodeSniffer_File $phpcsFile, $stackPtr) {
-        $this->processVariable($phpcsFile, $stackPtr);
+    protected function processVariableInString(PHP_CodeSniffer_File $phpcsfile, $stackptr) {
+        $this->processVariable($phpcsfile, $stackptr);
     }
 }
